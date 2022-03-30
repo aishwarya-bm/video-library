23b4ea53d30e5f39ef3666230734985064a36078
aishwarya-bm <biradaraishwarya@gmail.com>
2022-03-30 19:48:21 +0530

    removed commented code

--- src/components/navpills/Navpills.jsx
+++ src/components/navpills/Navpills.jsx
@@ -5,13 +5,12 @@ export default function Navpills() {
     <>
       <div className="nav-pills d-flex">
         <ul className="list list-no-bullet children-center">
-          {/* {!stateUser.isLoggedIn && ( */}
           <li className="list-item">
             <Link to="/signup" className="btn btn-link nav-btn ">
               LOGIN
             </Link>
           </li>
-          {/* )} */}
+
           <li className="list-item nav-item">
             <Link to="/" className="fa fa-solid fa-home"></Link>
           </li>
@@ -20,9 +19,6 @@ export default function Navpills() {
               <Link to="/wishlist">
                 <i className="fa fa-solid fa-heart nav-btn"></i>
               </Link>
-              {/* {stateUser.isLoggedIn && wishlistSize > 0 && (
-                <span className="badge">{wishlistSize}</span>
-              )} */}
             </span>
           </li>
           <li className="list-item nav-item">
@@ -30,29 +26,13 @@ export default function Navpills() {
               <Link to="/cart">
                 <i className="fa fa-shopping-cart nav-btn"></i>
               </Link>
-              {/* {stateUser.isLoggedIn && cartSize > 0 && (
-                <span className="badge">{cartSize}</span>
-              )} */}
             </span>
           </li>
         </ul>
-        {/* commented since its not yet implemented */}
-        {/* {localStorage.userToken && (
-          <Link
-            to="/profile"
-            className="fa fa-user btn btn-link nav-btn"
-          ></Link>
-        )} */}
-        {/* {stateUser.isLoggedIn && ( */}
+
         <Link to="/signup">
-          <i
-            className="fa fas fa-sign-out-alt btn btn-link nav-btn"
-            // onClick={() =>
-            //   signoutUser(dispatchCart, dispatchWishlist, dispatchUser)
-            // }
-          ></i>
+          <i className="fa fas fa-sign-out-alt btn btn-link nav-btn"></i>
         </Link>
-        {/* )} */}
       </div>
     </>
   );
